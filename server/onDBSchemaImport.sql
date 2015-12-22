fdf015cde4807cabbd3b764947ba67315c53e676
GregDungca <greg.dungca@gmail.com>
2015-12-21 18:22:25 -0800

    Added status to wandoo in database. Progress on check for public/images directory.

--- server/onDBSchemaImport.sql
+++ server/onDBSchemaImport.sql
@@ -2,4 +2,10 @@ drop database if exists `wandoo`;
 create database `wandoo`;
 use wandoo;
 
+INSERT INTO `wandoo_status` (`status`,`description`) VALUES
+('A','Active and visible on feed'),
+('P','Passive wandoo that is no longer visible on feed, but has an active room'),
+('E','Expired wandoo that is no longer visible on feed and has no active room'),
+;
+
 -- change all DECIMAL to DECIMAL(13,10)
--- server/util.js
+++ server/util.js
@@ -2,15 +2,37 @@ var fs = require('fs');
 
 module.exports = {
   isoDateToMySQL : function (date) {
+    console.log(date.substring(0,10) + ' ' + date.substring(11, 19));
     return date.substring(0,10) + ' ' + date.substring(11, 19);
   },
 
+  checkDir : function (path, callback) {
+    fs.readdir(path, function () {
+
+    })
+  },
+
   writeImage : function (path, image) {
-    fs.writeFile(__dirname + '/public' + path, new Buffer(image, 'base64'), function (err) {
-      if (err) {
-        throw err;
-      } 
-      console.log('Image successfully saved to the server');
+
+    var writeTheImage = function () {
+      fs.writeFile(__dirname + '/public' + path, new Buffer(image, 'base64'), function (err) {
+        if (err) {
+          throw err;
+        } 
+        console.log('Image successfully saved to the server');
+      });
+    }
+
+    fs.stat('/public/images', function (stats) {// check if the public directory exists
+      console.log('stats',files);
+      if (!stats.isDirectory()) { // if it doesn't, make that directory
+        fs.mkdir(__dirname + '/public' + '/images', function () {
+          writeTheImage();
+        });
+      } else {
+        writeTheImage();
+      }
+      
     });
   } 
 
