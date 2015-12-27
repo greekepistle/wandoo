c1d686fa161f4c92f61ec062eee1f24c6d062ae2
GregDungca <greg.dungca@gmail.com>
2015-12-26 23:12:51 -0800

    Fixed user API bug. Progress on more room deletion changes.

--- server/controllers/room.js
+++ server/controllers/room.js
@@ -78,9 +78,19 @@ module.exports = {
   },
 
   delete : function (req, res) {
-    room.delete(req.params.roomID, function (err, result) {
-      deleteQueryCB(err, result, res);
-    });
+    if (req.params.roomID) {
+      room.delete(req.params.roomID, function (err, result) {
+        deleteQueryCB(err, result, res);
+      });
+    } else {
+      if (!req.body.wandooIDs) {
+        res.send('Invalid parameters');
+        console.log('Does this get output?');
+      }
+      room.deleteByWandoo(req.body.wandooIDs, function(err, result) {
+        deleteQueryCB(err, result, res);
+      })
+    }
   },
 
   put : function (req, res) {
