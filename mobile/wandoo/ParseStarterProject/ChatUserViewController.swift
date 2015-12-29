aa94e2c4c46196788a767075d7440e5f6245536c
Manu <manu.mish@gmail.com>
2015-12-28 20:23:45 -0800

    Chat User View Controller added

---
+++ mobile/wandoo/ParseStarterProject/ChatUserViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ChatUserViewController.swift
+//  ParseStarterProject-Swift
+//
+//  Created by Manu Mishra on 12/28/15.
+//  Copyright © 2015 Parse. All rights reserved.
+//
+
+import UIKit
+
+class ChatUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Users"
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
