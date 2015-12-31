230806e2d594c3f59fdee617e00cca26997cb10c
GregDungca <greg.dungca@gmail.com>
2015-12-30 20:26:57 -0800

    implemented db disconnect handler

--- server/db.js
+++ server/db.js
@@ -1,13 +1,31 @@
 var mysql = require('mysql');
 var config = require('./config/config');
 
-db = mysql.createConnection({
+var dbConfig = {
   host: config.dbHost,
   user: config.dbUser,
   password: config.dbPassword,
   database: config.db,
   timezone: 'Z'
-});
+}
+
+var handleDisconnect = function (client) {
+  client.on('error', function (err) {
+    if (!err.fatal) {
+      return;
+    }
+    if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+      throw err;
+    } 
+    console.error('Reconnecting lost MySQL connection: ' + err.stack);
+    db = mysql.createConnection(dbConfig);
+    handleDisconnect(db);
+  });
+};
+
+var db = mysql.createConnection(dbConfig);
+
+handleDisconnect(db);
 
 // db.connect(function(err) {
 //   if (err) {
@@ -17,8 +35,29 @@ db = mysql.createConnection({
 //   console.log('Database successfully connected');
 // });
 
-db.on('error', function(err) {
-  console.log(err.code); // 'ER_BAD_DB_ERROR'
-});
+// db.on('error', function(err) {
+//   console.log(err.code); // 'ER_BAD_DB_ERROR'
+// });
+
+
 
 module.exports = db;
+
+
+// connection pool start
+
+// var mysql = require('mysql');
+// var pool  = mysql.createPool({
+//   connectionLimit : 10,
+//   host            : config.dbHost,
+//   user            : config.dbUser,
+//   password        : config.db
+// });
+
+// pool.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//   if (err) throw err;
+
+//   console.log('The solution is: ', rows[0].solution);
+// });
+
+// connection pool end
