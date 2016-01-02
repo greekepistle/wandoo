fb5a6880264a227f8cd4b3da93e9b68e5af01265
GregDungca <greg.dungca@gmail.com>
2016-01-01 22:40:32 -0800

    Put proper config file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,11 @@
 // PRODUCTION CONFIGURATION
 
 module.exports = {
-  port: 8000,
-  dbUser : 'b312350c551a04',
-  dbPassword : '130f65d5',
-  dbHost : 'us-cdbr-iron-east-03.cleardb.net',
-  db : 'heroku_97bae8bf3d1f5c7',
-  serverURL : 'https://wandoo-hs5abf-5804.herokuapp.com',
+  port: process.env.PORT,
+  dbUser : process.env.DBUSER,
+  dbPassword : process.env.DBPASS,
+  dbHost : process.env.DBHOST,
+  db : process.env.DB,
+  serverURL : process.env.SERVERURL,
 }
+
