aaa14030f871ad5ffe8b026b650f7307045eae29
Brian Kwon <brian.y.kwon@gmail.com>
2016-02-03 21:24:56 -0800

    fix employer string bug

--- mobile/wandoo/ParseStarterProject/DetailsViewController.swift
+++ mobile/wandoo/ParseStarterProject/DetailsViewController.swift
@@ -62,7 +62,7 @@ class DetailsViewController: UIViewController {
                         self.profileInfo.text! += jobTitle
                         self.profileInfo.text! += " at " + employer
                     } else {
-                        self.profileInfo.text! += " at " + employer
+                        self.profileInfo.text! += employer
                     }
                 }
                 
