f151db1f0eb41f98d0f61cde05417a755d00e104
Brian Kwon <brian.y.kwon@gmail.com>
2016-02-04 00:09:15 -0800

    OMG spelled "thursday" wrong

--- mobile/wandoo/ParseStarterProject/WandooModel.swift
+++ mobile/wandoo/ParseStarterProject/WandooModel.swift
@@ -32,7 +32,7 @@ class WandooModel {
         "Monday": "Tuesday",
         "Tuesday": "Wednesday",
         "Wednesday": "Thursday",
-        "Thursay": "Friday",
+        "Thursday": "Friday",
         "Friday": "Saturday",
         "Saturday": "Sunday",
     ]
@@ -174,6 +174,9 @@ class WandooModel {
         let splitFormattedDate = wandooFormattedDate.characters.split{$0 == " "}.map(String.init)
         
         if  todayIntDay < wandooIntDay {
+            print(wandooIntDay)
+            print(todayIntDay)
+            print(todayDay)
             if wandooIntDay! == todayIntDay! + 1 || String(tomorrowDays[todayDay]!) == splitFormattedDate[0] {
                 result = "Tomorrow at " + splitFormattedDate[1]
             } else {
